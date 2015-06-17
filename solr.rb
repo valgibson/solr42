51d2f2172cc0da027cabc93c65b434d07862916f
Valerie Gibson <valerie@triquanta.nl>
2015-06-17 22:17:34 +0200

    Solr 4.2.0 version

---
+++ solr.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class Solr < Formula
+  homepage 'http://lucene.apache.org/solr/'
+  url 'https://archive.apache.org/dist/lucene/solr/4.2.0/solr-4.2.0.tgz'
+  sha1 '3525a424be3c7f5de27ea1ab60b267cc0670b6c4'
+
+  def script; <<-EOS.undent
+    #!/bin/sh
+    if [ -z "$1" ]; then
+      echo "Usage: $ solr path/to/config/dir"
+    else
+      cd #{libexec}/example && java -server $JAVA_OPTS -Dsolr.solr.home=$1 -jar start.jar
+    fi
+    EOS
+  end
+
+  def install
+    libexec.install Dir['*']
+    (bin+'solr').write script
+  end
+
+  def caveats; <<-EOS.undent
+    To start solr:
+      solr path/to/solr/config/dir
+
+    See the solr homepage for more setup information:
+      brew home solr
+    EOS
+  end
+end
